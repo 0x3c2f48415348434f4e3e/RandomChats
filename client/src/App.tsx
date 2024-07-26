645c7e3559989af484ac055343b75543c57e476c
Benjamin <benjamin>
2024-07-26 15:49:00 +0100

    Configured Tailwind, ShadCN (Optional), magicUI and Typescript. Will begin Home page

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+//import './App.css';
+import RandomChatsShimmerButton from './component/RandomChatsShimmerButton';
+
+function App() {
+  return (
+    <div className="App">
+      <RandomChatsShimmerButton />
+    </div>
+  );
+}
+
+export default App;
