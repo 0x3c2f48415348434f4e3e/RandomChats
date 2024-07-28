15c2b1f13d86d7638c677c62686c98b92e61d8d5
Benjamin <benjamin>
2024-07-28 15:43:57 +0100

    Update index.tsx to manage routes

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,18 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
+//react router
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import Home from './component/routes/Home';
+import About from './component/routes/About';
+import Blog from './component/routes/Blog';
+import Contact from './component/routes/Contact';
+import Login from './component/routes/Login';
+import Register from './component/routes/Register';
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <Home />,
+  },
+  {
+    path:"/about",
+    element: <About />,
+  },
+  {
+    path:"/contact",
+    element: <Contact />,
+  },
+  {
+    path:"/blog",
+    element: <Blog />,
+  },
+  {
+    path:"/login",
+    element: <Login />,
+  },
+  {
+    path:"/register",
+    element: <Register />,
+  }
+])
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
-
+//<App />
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
