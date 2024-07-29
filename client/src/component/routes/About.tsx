266b1a02fe3fc71ad6ded4580e8b2d7b45df638d
Benjamin <benjamin>
2024-07-29 16:45:09 +0100

    Implemented TOGGLE. Remove toggle from Home Screen.

--- client/src/component/routes/About.tsx
+++ client/src/component/routes/About.tsx
@@ -1,8 +1,26 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
+import ToggleBetweenLightAndDark from "../ToggleBetweenLightAndDark";
 
 const About = ()=>{
+    //true will be light and false is dark
+    const [selected, setSelected] = useState(true);
+    const handleToggle =()=>{
+        setSelected(!selected);
+    }
+
+    //The selected is our varibale. by default light is true and dark is false (I don't know why i did it like this)
+    const BACKGROUNDCOLOR = selected?"white":"black";
+    const TEXTCOLOR = selected?"black":"white";
+    useEffect(()=>{
+        document.body.style.backgroundColor = BACKGROUNDCOLOR;
+        document.body.style.color = TEXTCOLOR;
+    }, [BACKGROUNDCOLOR,TEXTCOLOR])
     return(
-        <h1>Welcome to the About page</h1>
+        //lassName={`h-full ${selected ? "bg-white" : "bg-black"}`}
+        <div>
+            <ToggleBetweenLightAndDark selected={selected} Toggle={handleToggle} />
+            <h1>Welcome to the About page</h1>
+        </div>
     )
 }
 
