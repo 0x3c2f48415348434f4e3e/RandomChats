84f52d743a927c27d731a8613ed6be4b3eed9046
Benjamin <benjamin>
2024-08-01 14:48:25 +0100

    Make icon smaller

--- client/src/component/ToggleBetweenLightAndDark.tsx
+++ client/src/component/ToggleBetweenLightAndDark.tsx
@@ -2,37 +2,45 @@ import { motion } from "framer-motion";
 //import { useState } from "react";
 import { FiMoon, FiSun } from "react-icons/fi";
 
+
+interface ToggleBetweenLightAndDarkProps {
+  selected: boolean;
+  Toggle: () => void;
+}
+
+//<span className="relative z-10">Light</span>
+//<span className="relative z-10">Dark</span>
 const TOGGLE_CLASSES =
   "text-sm font-medium flex items-center gap-2 px-3 md:pl-3 md:pr-3.5 py-3 md:py-1.5 transition-colors relative z-10";
 //{selected: string, setSelected}
-const ToggleBetweenLightAndDark = (props:any)=>{
+const ToggleBetweenLightAndDark : React.FC<ToggleBetweenLightAndDarkProps> = ({ selected, Toggle }) =>{
     return(
         //relative flex w-fit items-center rounded-full content-center
         <div className="relative flex items-center w-fit">
             <button className={`${TOGGLE_CLASSES} ${
-          props.selected ? "text-white" : "text-slate-300"
+          selected ? "text-white" : "text-slate-300"
         }`}
         onClick={()=>{
-            props.Toggle();
+            Toggle();
         }}
         >
             <FiMoon className="relative z-10 text-lg md:text-sm"/>
-            <span className="relative z-10">Light</span>
+            
         </button>
         <button
         className={`${TOGGLE_CLASSES} ${
-          !props.selected ? "text-white" : "text-slate-800"
+          !selected ? "text-white" : "text-slate-800"
         }`}
         onClick={() => {
-          props.Toggle();
+          Toggle();
         }}
       >
         <FiSun className="relative z-10 text-lg md:text-sm" />
-        <span className="relative z-10">Dark</span>
+        
       </button>
       <div
         className={`absolute inset-0 z-0 flex ${
-          !props.selected  ? "justify-end" : "justify-start"
+          !selected  ? "justify-end" : "justify-start"
         }`}
       >
         <motion.span
